9fdf8f46ef4af0235861dfdb7a541177c89c2687
maegann21 <maegan.wqtan@gmail.com>
2025-01-15 14:46:58 +0800

    css for index

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width,
     initial-scale=1.0">
     <title>Robotic Course Managem System</title>
+    <link rel ="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="body-home">
     "Welcome to the Robotic Course Management System"
 </body>
 </html>
